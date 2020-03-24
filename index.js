6ee2465d430e6c661899fa46faf81a005b4a132c
Alouiscious Media <info@reneesoul.com>
2020-03-24 16:57:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize( string ) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize( string ) {
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '')
+  }
+
+  static titleize( phrase ) {
+    let xclsnWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    let evalPhrase = phrase.split(" ");
+    
+    return evalPhrase.map((word, i) => {
+      if (i == 0) {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+        
+      }
+      else if (!xclsnWords.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      }
+      else {
+        return word
+      }
+  
+    }).join(" ")
+  }
 }
